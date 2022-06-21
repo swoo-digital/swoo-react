57fc660e216174d00b92a53b02762fcfeb4056e9
Ivan Driuk <i.driuk@sloboda-studio.com>
2022-06-21 12:32:32 +0300

    add Counter to App, fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders', () => {
-  render(<App />);
   expect(1).toBe(1);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div>
+      <Counter />
     </div>
   );
 }
